17a8252b5e2501f34ac7d66c5db3758684b78efb
Mikołaj Biel <mbiel@mailbox.org>
2018-05-26 12:33:59 +0200

    Update git config

--- _meta/scripts/setup-git.sh
+++ _meta/scripts/setup-git.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+DOTFILES_DIR=$(realpath `dirname $0`/../../)
+TEMPLATE_FILE=$DOTFILES_DIR/git/gitconfig
+DESIRED_PATH=$HOME/.gitconfig
+
 setup-git() {
     echo "Enter your full name (for git identifaction):"
     read USER_FULLNAME
     echo "Enter your email address (for git identification):"
     read USER_EMAIL
-    cat ../../git/gitconfig |
-        sed "/name =\W*$/  s/name.*$/name = "$USER_FULLNAME"/" |
-        sed "/email =\W*$/ s/email.*$/email = "$USER_EMAIL"/" > $HOME/.gitconfig;
+    cat $TEMPLATE_FILE |
+        sed "/name =\W*$/  s/name.*$/name = $USER_FULLNAME/" |
+        sed "/email =\W*$/ s/email.*$/email = $USER_EMAIL/" > $DESIRED_PATH
 }
 
 GIT_FILLED_FIELDS=$(test -f $HOME/.gitconfig && cat $HOME/.gitconfig | grep -e "\(name\|email\) = \w\+" | wc -l || echo 0)
