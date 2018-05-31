8cab24ab5c9bef6053ab2635e8a013eb0df89354
Mikołaj Biel <mbiel@mailbox.org>
2018-05-31 17:54:02 +0200

    Split configuration into profiles

---
+++ .meta/scripts/install-profiles.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+CONFIG_FILENAME="install.conf.yaml"
+
+META_DIR=".meta"
+PROFILES_DIR="profiles"
+
+DOTBOT_DIR="dotbot"
+DOTBOT_BIN="bin/dotbot"
+
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+
+
+for PROFILE_FILE in ${@}; do
+    while IFS= read -r config; do
+        CONFIGS+=" ${config}"
+    done < "${BASE_DIR}/${META_DIR}/${PROFILES_DIR}/$PROFILE_FILE"
+    shift
+done
+
+
+for config in ${CONFIGS}; do
+    echo -e "\nConfigure $config"
+    configContent="$(<"${BASE_DIR}/${config}/${CONFIG_FILENAME}")"
+    "${BASE_DIR}/${META_DIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASE_DIR}" -c <(echo -e "$configContent")
+done
