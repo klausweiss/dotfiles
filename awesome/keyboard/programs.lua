f125adea0c0c08dfd0d4d42a718b3ec5d173dab2
Mikołaj Biel <mbiel@mailbox.org>
2018-06-13 13:44:43 +0200

    [awesome] Change terminal, floating reload and select master keys

--- awesome/keyboard/programs.lua
+++ awesome/keyboard/programs.lua
@@ -3,9 +3,9 @@ local awful = require("awful")
 
 -- Standard program
 local globalkeys = gears.table.join(
-  awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
+  awful.key({ modkey, "Shift"   }, "Return", function () awful.spawn(terminal) end,
     {description = "open a terminal", group = "launcher"}),
-  awful.key({ modkey, "Control" }, "r", awesome.restart,
+  awful.key({ modkey, "Shift"   }, "r", awesome.restart,
     {description = "reload awesome", group = "awesome"}),
   awful.key({ modkey, "Shift"   }, "q", awesome.quit,
     {description = "quit awesome", group = "awesome"})
