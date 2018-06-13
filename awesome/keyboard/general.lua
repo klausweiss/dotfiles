e12ae1d89ceaca04f2ec8dd6fdd17d7aaefbe19c
Mikołaj Biel <mbiel@mailbox.org>
2018-06-13 10:45:01 +0200

    Add volume control keybindings (amixer)

--- awesome/keyboard/general.lua
+++ awesome/keyboard/general.lua
@@ -4,13 +4,29 @@ local hotkeys_popup = require("awful.hotkeys_popup").widget
 
 local menu = require("menu")
 
-local globalkeys = gears.table.join(
+local general_keys = gears.table.join(
   awful.key({ modkey,       }, "s",    hotkeys_popup.show_help,
             {description="show help", group="awesome"}),
   awful.key({ modkey,       }, "Escape", awful.tag.history.restore,
             {description = "go back", group = "tag"}),
   awful.key({ modkey,       }, "w", function () menu.mainmenu:show() end,
             {description = "show main menu", group = "awesome"})
+)
+
+local special_keys = gears.table.join(
+  awful.key({               }, "XF86AudioRaiseVolume",
+    function ()
+      awful.util.spawn("amixer sset Master 4%+")
+    end,    {description = "raise volume"}),
+  awful.key({               }, "XF86AudioLowerVolume",
+    function ()
+      awful.util.spawn("amixer sset Master 4%-")
+    end,    {description = "lower volume"})
+)
+
+local globalkeys = gears.table.join(
+  general_keys,
+  special_keys
 )
 
 return {
