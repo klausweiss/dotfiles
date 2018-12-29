c6ee7d6a68257c06e45919092b3bba2227966b23
Mikołaj Biel <mbiel@mailbox.org>
2018-12-29 22:13:10 +0100

    [awesome] Switch config to awesome-ez

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,32 +1,93 @@
-require("errors_handler")
-require("globals")
-
-local awful = require("awful")
-local menubar = require("menubar")
-
-local layouts = require("layouts")
-local screens = require("screens")
-local mouse = require("mouse")
-local keyboard = require("keyboard.all")
-local rules = require("rules")
-local signals = require("signals")
-
-require("awful.autofocus")
-require("awful.hotkeys_popup.keys")
+-- theme
+local beautiful = require("beautiful")
+local gears = require("gears")
+beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua") 
 
 
--- Keyboard
-root.keys(keyboard.globalkeys)
--- Layouts
-awful.layout.layouts = layouts
--- Mouse
-root.buttons(mouse.rootbuttons)
--- Menu
-menubar.utils.terminal = terminal
--- Rules
-awful.rules.rules = rules
--- Signals
-signals.connect_client_signals(signals.client_signals)
-signals.connect_screen_signals(signals.screen_signals)
--- Screens
-awful.screen.connect_for_each_screen(screens.for_each_screen)
+local ez = require("awesome-ez") 
+
+ez.layout.layouts = {
+   tile,
+   max,
+   floating,
+} 
+
+ez.tags.tags = {"1", "2", "3", "4", "5", "6", "7"}
+
+ez.mouse.desktop_wheel_down = show_prev_tag
+ez.mouse.desktop_wheel_up   = show_next_tag
+ez.mouse.client_click              = focus_client
+ez.mouse.client_click[{alt}]       = move_client
+ez.mouse.client_right_click[{alt}] = resize_client
+
+-- layouts
+ez.keyboard.global[{alt,        " "}] = next_layout
+ez.keyboard.global[{alt, shift, " "}] = prev_layout
+
+-- windows
+ez.keyboard.global[{alt,        tab}]   = focus_next_client
+ez.keyboard.global[{alt, shift, tab}]   = focus_prev_client
+ez.keyboard.client[{alt, shift, "f"}]   = toggle_fullscreen_client
+ez.keyboard.client[{alt, shift, "m"}]   = toggle_maximize_client
+ez.keyboard.client[{alt, "w"}]          = close_client
+ez.keyboard.client[{alt, shift, enter}] = select_main_client
+
+-- awesome
+ez.keyboard.global[{alt, shift, "r"}]  = awesome.restart
+ez.keyboard.global[{alt, shift, "q"}]  = awesome.quit
+
+-- launcher
+local launcher = "rofi -show run"
+ez.keyboard.global[{ctrl,  " "}] = run(launcher)
+ez.keyboard.global[{super, " "}] = require("menubar").show
+
+-- multiple monitors
+ez.keyboard.global[{alt, "m"}] = focus_left_screen
+ez.keyboard.global[{alt, ","}] = move_client_to_another_screen
+ez.keyboard.global[{alt, "."}] = focus_right_screen
+
+-- tags
+ez.keyboard.tags[{     ctrl       }] = show_tag_by_index
+ez.keyboard.tags[{alt, ctrl       }] = toggle_tag_by_index
+ez.keyboard.tags[{     ctrl, shift}] = move_focused_client_to_tag
+ez.keyboard.tags[{alt, ctrl, shift}] = toggle_tag_on_focused_client
+
+-- programs
+local terminal         = "xterm"
+local file_manager     = "thunar"
+local internet_browser = "firefox"
+local screen_lock      = "slock"
+ez.keyboard.global[{alt, ctrl, enter}] = run(terminal)
+ez.keyboard.global[{alt, shift, "e"}]  = run(file_manager)
+ez.keyboard.global[{alt, shift, "b"}]  = run(internet_browser)
+ez.keyboard.global[{alt, shift, "l"}]  = run(screen_lock)
+
+-- system
+ez.keyboard.global[{"XF86MonBrightnessUp"}]   = run("light -A 3")
+ez.keyboard.global[{"XF86MonBrightnessDown"}] = run("light -U 3")
+ez.keyboard.global[{"XF86AudioRaiseVolume"}]  = run("amixer sset Master 4%+")
+ez.keyboard.global[{"XF86AudioLowerVolume"}]  = run("amixer sset Master 4%-")
+
+ez.wibar.left = {
+   launcher_menu,
+   taglist,
+}
+
+ez.wibar.middle = {
+   tasklist,
+}
+
+local volumebar  = require("widgets.volumebar")
+local batterybar = require("widgets.battery-widget")
+
+ez.wibar.right = {
+   function () return volumebar end,
+   batterybar,
+   tray,
+   clock,
+   layouts_switcher,
+}
+
+ez.client.focus_follow_mouse = true
+
+ez.setup()
