894a1d513e9d3cf3adcac56569adeffecfe47818
Mikołaj Biel <mbiel@mailbox.org>
2018-12-29 22:15:18 +0100

    [emacs] Add evil neotree keybindings

--- emacs/init.el
+++ emacs/init.el
@@ -45,6 +45,15 @@
 (use-package evil
   :config
   (evil-mode t)
+    (evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+    (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+    (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+    (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+    (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
+    (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
+    (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
+    (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
+    (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
   )
 
 (use-package evil-magit)
