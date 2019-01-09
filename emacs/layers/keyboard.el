be09b249ac125cf5d0ce641fe396df8d82f6f29b
Mikołaj Biel <mbiel@mailbox.org>
2019-01-09 19:17:05 +0100

    [emacs] Bind org mode shortcuts

--- emacs/layers/keyboard.el
+++ emacs/layers/keyboard.el
@@ -9,3 +9,9 @@
   )
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; The following lines are always needed.  Choose your own keys.
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-switchb)
