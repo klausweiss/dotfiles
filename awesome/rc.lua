b223664b76074157196d08b1c5788a45d6e08ffe
Mikołaj Biel <mbiel@mailbox.org>
2019-01-14 14:28:27 +0100

    [awesome] Set theme using ez

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,9 +1,3 @@
--- theme
-local beautiful = require("beautiful")
-local gears = require("gears")
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua") 
-
-
 local ez = require("ez")
 
 ez.theme.wallpaper = "~/.config/wallpaper"
