6d0c7d673e5376dda1be1dc790de53efca714e13
Mikołaj Biel <mbiel@mailbox.org>
2019-02-07 19:03:02 +0100

    [emacs] Open edit navigation buffer in the existing frame

--- emacs/layers/git.el
+++ emacs/layers/git.el
@@ -1,3 +1,5 @@
 (use-package magit
   :config
-  (setq magit-display-buffer-function 'magit-display-buffer-traditional))
+  (setq magit-display-buffer-function #'magit-display-buffer-traditional)
+  (setq ediff-window-setup-function #'ediff-setup-windows-plain)
+  )
