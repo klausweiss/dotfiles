f02d1f52b99faee89d86052c52a8ead1d69eacc7
Mikołaj Biel <mbiel@mailbox.org>
2019-02-08 14:08:12 +0100

    [emacs] Add /C-c p/ projectile bindings

--- emacs/layers/projects.el
+++ emacs/layers/projects.el
@@ -1,6 +1,16 @@
 (use-package projectile
   :config
   (projectile-global-mode)
+
+  (defalias '@project-switch #'helm-projectile-switch-project)
+  (defalias '@project-find-file #'helm-projectile-find-file)
+  (defalias '@project-switch-buffer #'helm-projectile-switch-to-buffer)
+  (defalias '@project-tree #'neotree-project-dir)
+
+  :bind (("C-c p p" . #'@project-switch)
+	 ("C-c p f" . #'@project-find-file)
+	 ("C-c p b" . #'@project-switch-buffer)
+	 ("C-c p t" . #'@project-tree))
   )
 
 (use-package helm-projectile
