93f67a55d135ab444dc9c31cbcadcbd5c1bda877
Mikołaj Biel <mbiel@mailbox.org>
2019-02-08 14:08:57 +0100

    [emacs] Add /C-c g/ for magit status

--- emacs/layers/git.el
+++ emacs/layers/git.el
@@ -1,4 +1,5 @@
 (use-package magit
+  :bind (("C-c g" . #'magit-status))
   :config
   (setq magit-display-buffer-function #'magit-display-buffer-traditional)
   (setq ediff-window-setup-function #'ediff-setup-windows-plain)
