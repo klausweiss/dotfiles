b55963d1a1b853e728a1d27d810635e04b288fd1
Mikołaj Biel <mbiel@mailbox.org>
2019-03-07 07:19:04 +0100

    [emacs] Add writing layer

---
+++ emacs/layers/writing.el
@@ -0,0 +1,11 @@
+(defun linum-mode-disable ()
+  (linum-mode 0))
+
+(defun text-scale-set-big ()
+  (text-scale-set 2))
+
+(use-package writeroom-mode
+  :init
+  (add-hook 'writeroom-mode-hook #'linum-mode-disable)
+  (add-hook 'writeroom-mode-hook #'text-scale-set-big)
+  )
