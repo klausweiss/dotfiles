3bc32e562ac4c6e671b9ce1ad9491290e5fe7f80
Mikołaj Biel <mbiel@mailbox.org>
2019-05-11 23:57:16 +0200

    [emacs] Increase minimum-company-prefix-length to 2

--- emacs/layers/autocomplete.el
+++ emacs/layers/autocomplete.el
@@ -24,7 +24,7 @@
   :config
   (add-hook 'after-init-hook 'global-company-mode)
   (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1)
+  (setq company-minimum-prefix-length 2)
   (setq company-dabbrev-downcase nil)
   )
 
