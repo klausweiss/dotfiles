f326578e5ca540fb9d2de126dcc5de48f4321110
Mikołaj Biel <mbiel@mailbox.org>
2020-02-29 20:42:20 +0100

    [emacs] Dim buffer with code (solaire-mode)

---
+++ emacs/packages/ui/buffers.el
@@ -0,0 +1,9 @@
+;; dim buffers with code
+
+(use-package solaire-mode
+  :hook
+  ((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
+  (minibuffer-setup . solaire-mode-in-minibuffer)
+  :config
+  (solaire-global-mode +1)
+)
