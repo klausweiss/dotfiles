1eaa121cce6b8401a3088002296c067af53e1bfa
Mikołaj Biel <mbiel@mailbox.org>
2020-08-20 12:20:53 +0200

    [emacs] Bind M-0 in magit-status-mode to M-1

--- emacs/normal/normal-magit.el
+++ emacs/normal/normal-magit.el
@@ -2,6 +2,7 @@
 
 (with-eval-after-load 'magit
   (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+  (define-key magit-status-mode-map (kbd "M-0") #'magit-section-show-level-1-all)
   )
 
 (provide 'normal-magit)
