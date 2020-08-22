397b5850cb7af10d632a1a135fe276d52839cee3
Mikołaj Biel <mbiel@mailbox.org>
2020-08-22 13:24:10 +0200

    [emacs] Autoload company-box

--- emacs/normal/normal-completion.el
+++ emacs/normal/normal-completion.el
@@ -1,21 +1,26 @@
+(require 'normal-autoload)
+
+(autoload-all "../lib/company-box/company-box"
+	      #'company-box-mode
+	      )
+
 (require 'company)
 
-(require 'company-box)
-(add-hook 'company-mode-hook #'company-box-mode)
+(with-eval-after-load 'company
+  (add-hook 'company-mode-hook #'company-box-mode)
+  (setq company-backends '(company-capf
+			   company-dabbrev
+			   )
+	company-idle-delay 0
+	company-dabbrev-downcase nil
+	tab-always-indent 'complete
+	company-quickhelp-delay 0.5
 
-(setq company-backends '(company-capf
-			 company-dabbrev
-			 )
-      company-idle-delay 0
-      company-dabbrev-downcase nil
-      tab-always-indent 'complete
-      company-quickhelp-delay 0.5
+	completion-function #'company-complete-common-or-cycle
+	)
+  (define-key company-mode-map [escape] #'company-abort)
 
-      completion-function #'company-complete-common-or-cycle
-      )
-
-(define-key company-mode-map [escape] #'company-abort)
-
-(global-company-mode t)
+  (global-company-mode t)
+  )
 
 (provide 'normal-completion)
