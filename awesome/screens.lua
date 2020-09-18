17dd7e624205d645ca891c44628753954c98faa8
Mikołaj Biel <mbiel@mailbox.org>
2020-09-18 21:59:44 +0200

    [awesome] Add comment explaining unused import

--- awesome/screens.lua
+++ awesome/screens.lua
@@ -1,6 +1,7 @@
 local awful = require("awful")
 
 local libtheme = require("lib/theme")
+-- the layouts import is required for each screen to have correct layout
 local layouts = require("layouts")
 local bars = require("lib/bars")
 local keyboard = require("keyboard")
