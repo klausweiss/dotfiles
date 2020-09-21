05946065a671b421801c83a2cbcef740f3d4a57e
Mikołaj Biel <mbiel@mailbox.org>
2020-09-21 20:13:26 +0200

    [emacs] Enable rustic-mode for rust files

--- emacs/normal/normal-languages.el
+++ emacs/normal/normal-languages.el
@@ -4,10 +4,27 @@
 (autoload-all "lsp"
 	      #'lsp
 	      )
+(autoload-all "rustic"
+	      #'rustic-mode
+	      )
 
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
 (add-hook 'lua-mode-hook #'lsp)
+
+(add-to-list 'auto-mode-alist '("\\.rs$" . rustic-mode))
+(add-hook 'rustic-mode-hook #'lsp)
+(with-eval-after-load 'rustic
+  (with-eval-after-load 'flycheck
+    (push 'rustic-clippy flycheck-checkers)
+    (setq rustic-flycheck-clippy-params "--message-format=json"
+          lsp-rust-analyzer-cargo-watch-command "clippy"
+          lsp-rust-analyzer-cargo-watch-enable t
+          lsp-rust-analyzer-cargo-all-targets t
+          lsp-rust-analyzer-cargo-watch-command "clippy"
+          lsp-rust-analyzer-cargo-watch-args "--all-features --tests"
+	  )
+    ))
 
 (add-hook 'perl-mode-hook #'lsp)
 (add-hook 'python-mode-hook #'lsp)
