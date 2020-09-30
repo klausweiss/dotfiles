69e039ba70bafce14589247b6d586c9aac26a293
Mikołaj Biel <mbiel@mailbox.org>
2020-09-30 21:18:45 +0200

    [emacs] Disable ivy-posframe

--- emacs/normal/normal-ivy.el
+++ emacs/normal/normal-ivy.el
@@ -3,7 +3,6 @@
 (require 'ivy)
 
 (with-eval-after-load 'ivy
-  (require 'ivy-posframe)
   (setq enable-recursive-minibuffers t
 	ivy-count-format "(%d/%d) "
 	ivy-re-builders-alist '((t . ivy--regex-plus))
@@ -11,13 +10,6 @@
 
   (define-key ivy-minibuffer-map [escape] 'minibuffer-keyboard-quit)
   (ivy-mode 1)
-  )
-
-(with-eval-after-load 'ivy-posframe
-  (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center))
-	ivy-posframe-parameters '((internal-border-width . 1))
-	)
-  (ivy-posframe-mode 1)
   )
 
 (provide 'normal-ivy)
