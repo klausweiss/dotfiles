85acfc7dab782ad7849deb9410615bfb4ae87e14
Mikołaj Biel <mbiel@mailbox.org>
2020-10-04 11:43:17 +0200

    [emacs] Unbind C-w in magit-status-mode-map

--- emacs/normal/normal-magit.el
+++ emacs/normal/normal-magit.el
@@ -3,6 +3,7 @@
 (with-eval-after-load 'magit
   (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
   (define-key magit-status-mode-map (kbd "M-0") #'magit-section-show-level-1-all)
+  (define-key magit-status-mode-map (kbd "C-w") nil)
   )
 
 (provide 'normal-magit)
