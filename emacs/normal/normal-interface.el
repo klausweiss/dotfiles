29e50db4484f1da0bc473418179e4c16741e43f5
Mikołaj Biel <mbiel@mailbox.org>
2020-10-04 11:44:36 +0200

    [emacs] Use mini-modeline instead of doom-modeline

--- emacs/normal/normal-interface.el
+++ emacs/normal/normal-interface.el
@@ -22,9 +22,24 @@
 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+(menu-bar-mode -1)
 (load-dark-theme)
 
-(require 'doom-modeline)
-(doom-modeline-mode 1)
+(require 'mini-modeline)
+(with-eval-after-load 'mini-modeline
+  (mini-modeline-mode 1)
+  (setq mini-modeline-r-format (list
+				"%* "  ;; modified marker
+				"%b "  ;; buffer name
+				"[%m] "  ;; mode
+				"%l (%p)"  ;; line position
+				)
+	mini-modeline-display-gui-line nil
+	mini-modeline-right-padding 1
+	mini-modeline-enhance-visual nil
+	)
+
+  (set-face-background 'mini-modeline-mode-line "#bdae93")
+  )
 
 (provide 'normal-interface)
