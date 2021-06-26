047d61d76764a75b238bfb7a5eb06b30d5ef8d64
Mikołaj Biel <mbiel@mailbox.org>
2021-06-26 15:00:31 +0200

    [awesome] Move most of programs to keyboard

--- awesome/programs.lua
+++ awesome/programs.lua
@@ -1,15 +1,4 @@
 return {
    terminal = "xterm",
    editor = "emacs",
-   file_manager = "nemo",
-   internet_browser = "firefox",
-   launcher = "rofi -show run",
-   mail_client = "thunderbird",
-   screen_lock = "i3lock -c 000000",
-   screenshooter = "flameshot gui",
-   power_menu = "rofi -show power-menu -modi power-menu:~/.dotfiles/rofi/rofi-power-menu/rofi-power-menu",
-
-   brightness_up = "light -A 4",
-   brightness_down = "light -U 4",
-   refresh_displays = "autorandr -c",
 }
