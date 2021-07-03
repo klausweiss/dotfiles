6b96625c65cbc50035462691549a24ce576d419e
Mikołaj Biel <mbiel@mailbox.org>
2021-07-03 23:09:21 +0200

    [nvim] Use gruvbox-material instead of gruvbox

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,10 @@
 local cmd = vim.cmd
 
 -- theme, powerline
-cmd "autocmd vimenter * ++nested colorscheme gruvbox"
+vim.api.nvim_set_var('gruvbox_material_sign_column_background', 'clear') -- transparent signcolumn
+cmd 'autocmd vimenter * ++nested colorscheme gruvbox-material'
 vim.api.nvim_set_var('airline_powerline_fonts', 1)
+-- line numbers
+cmd 'set number'
 
 
