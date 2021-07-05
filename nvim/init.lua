90b719e6db2a42075a86e8a96774a6f4668e6585
Mikołaj Biel <mbiel@mailbox.org>
2021-07-05 23:16:42 +0200

    [nvim] Set guifont

--- nvim/init.lua
+++ nvim/init.lua
@@ -52,10 +52,11 @@ require'compe'.setup {
   };
 }
 
--- theme, powerline
+-- theme, powerline, font
 vim.api.nvim_set_var('gruvbox_material_sign_column_background', 'clear') -- transparent signcolumn
 cmd 'autocmd vimenter * ++nested colorscheme gruvbox-material'
 vim.api.nvim_set_var('airline_powerline_fonts', 1)
+cmd 'set guifont=JetBrains\\ Mono'
 -- line numbers
 cmd 'set number'
 
