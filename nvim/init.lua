4a211ee97e0317e7e95f97b94c73026474c720ed
Mikołaj Biel <mbiel@mailbox.org>
2022-09-15 23:15:58 +0200

    [nvim] Add treesitter config

--- nvim/init.lua
+++ nvim/init.lua
@@ -264,6 +264,35 @@ require('gitsigns').setup {
 
 require("trouble").setup {}
 
+-- tree-sitter
+require 'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "lua", "haskell" },
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+    -- list of language that will be disabled
+    disable = {},
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<leader>ee",
+      node_incremental = "<leader>ee",
+      scope_incremental = "<leader>es",
+      node_decremental = "<leader>ew",
+    },
+  },
+}
+
 -- lsp
 local nvim_lsp = require('lspconfig')
 
