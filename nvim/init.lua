4722131e5a60c15b6b8a67fd5553fc1fc9df7c62
Mikołaj Biel <mbiel@mailbox.org>
2022-09-17 01:25:09 +0200

    [nvim] TSEnable highlight for haskell

--- nvim/init.lua
+++ nvim/init.lua
@@ -281,7 +281,7 @@ require 'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
     -- list of language that will be disabled
-    disable = { "haskell" },
+    disable = {},
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
