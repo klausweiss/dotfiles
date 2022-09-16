6074aa23d86089c26775d7ff36a23dc3d374121b
Mikołaj Biel <mbiel@mailbox.org>
2022-09-16 22:18:25 +0200

    [nvim] Only insert cmp suggestion after it was selected

--- nvim/init.lua
+++ nvim/init.lua
@@ -105,7 +105,7 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Set `select` to `false` to only confirm explicitly selected items.
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
