762e3d17fb748a56b7c0794472e8d2a2bb90b13d
Mikołaj Biel <mbiel@mailbox.org>
2023-03-23 23:07:43 +0100

    [awesome] Add function for toggling sound sink

--- awesome/lib/api/sound/pulseaudio-pactl.lua
+++ awesome/lib/api/sound/pulseaudio-pactl.lua
@@ -13,10 +13,16 @@ local mute_output_cmd = "pactl set-sink-mute @DEFAULT_SINK@ 1"
 local unmute_output_cmd = "pactl set-sink-mute @DEFAULT_SINK@ 0"
 local toggle_mute_output_cmd = "pactl set-sink-mute @DEFAULT_SINK@ toggle"
 
-local get_sink_number_cmd = "pactl list sinks | grep 'Sink #.' | sed 's/.*\\([0-9]\\+\\).*/\\1/g' | head -n1"
+local get_sink_number_cmd =
+"pactl list sinks | grep 'Sink #.' -A1 | grep RUNNING -B1 | head -n1 | sed 's/.*\\([0-9]\\+\\).*/\\1/g' | head -n1"
+local get_all_sinks_numbers_cmd = "pactl list sinks | grep 'Sink #.' | sed 's/.*\\([0-9]\\+\\).*/\\1/g'"
 
-local set_output_type_to_headphones_cmd = function(sink_number) return "pactl set-sink-port " .. sink_number .. " analog-output-headphones" end
-local set_output_type_to_speakers_cmd = function(sink_number) return "pactl set-sink-port " .. sink_number .. " analog-output-speaker" end
+local set_output_type_to_headphones_cmd = function(sink_number) return "pactl set-sink-port " ..
+       sink_number .. " analog-output-headphones" end
+local set_output_type_to_speakers_cmd = function(sink_number) return "pactl set-sink-port " ..
+       sink_number .. " analog-output-speaker" end
+
+local set_sink_number_cmd = function(sink_number) return "pactl set-default-sink " .. sink_number end
 
 local get_output_volume_cmd = "pactl list sinks | grep -i 'volume.*front' | tail -n1"
 local get_input_volume_cmd = "pactl list sources | grep -i 'volume.*front' | tail -n1"
@@ -24,6 +30,16 @@ local get_input_volume_cmd = "pactl list sources | grep -i 'volume.*front' | tai
 local volume_up = function(x) return "pactl set-sink-volume @DEFAULT_SINK@ +" .. x .. "%" end
 local volume_down = function(x) return "pactl set-sink-volume @DEFAULT_SINK@ -" .. x .. "%" end
 
+-- split("a,b,c", ",") => {"a", "b", "c"}
+local function split(s, sep)
+   local fields = {}
+
+   local sep = sep or " "
+   local pattern = string.format("([^%s]+)", sep)
+   string.gsub(s, pattern, function(c) fields[#fields + 1] = c end)
+
+   return fields
+end
 
 function mk_sound_info()
    local sound_info = {
@@ -45,10 +61,10 @@ function mk_sound_info()
 
       awful.spawn.easy_async_with_shell
       (is_output_muted, function(o, e, r, code)
-	  self.output_muted = (code == 0)
+         self.output_muted = (code == 0)
 
-	  self._refreshing_output_muted = false
-	  self:emit_changed("output")
+         self._refreshing_output_muted = false
+         self:emit_changed("output")
       end)
    end
 
@@ -58,13 +74,13 @@ function mk_sound_info()
 
       awful.spawn.easy_async_with_shell
       (is_speakers_cmd, function(o, e, r, code)
-	  if code == 0 then
-	     self.output_device_type = "speakers"
-	  else
-	     self.output_device_type = "headphones"
-	  end
-	  self._refreshing_output_type = false
-	  self:emit_changed("output")
+         if code == 0 then
+            self.output_device_type = "speakers"
+         else
+            self.output_device_type = "headphones"
+         end
+         self._refreshing_output_type = false
+         self:emit_changed("output")
       end)
    end
 
@@ -74,10 +90,10 @@ function mk_sound_info()
 
       awful.spawn.easy_async_with_shell
       (is_input_muted, function(o, e, r, code)
-	  self.input_muted = (code == 0)
+         self.input_muted = (code == 0)
 
-	  self._refreshing_input_muted = false
-	  self:emit_changed("input")
+         self._refreshing_input_muted = false
+         self:emit_changed("input")
       end)
    end
 
@@ -95,12 +111,12 @@ function mk_sound_info()
 
       awful.spawn.easy_async_with_shell
       (get_output_volume_cmd, function(out, e, r, c)
-	  self.output_volume = get_volume_from_two_percents_string(out)
+         self.output_volume = get_volume_from_two_percents_string(out)
 
-	  self._refreshing_output_volume = false
-	  if not args.nonotify then
-	     self:emit_changed("output::volume")
-	  end
+         self._refreshing_output_volume = false
+         if not args.nonotify then
+            self:emit_changed("output::volume")
+         end
       end)
    end
 
@@ -111,12 +127,12 @@ function mk_sound_info()
 
       awful.spawn.easy_async_with_shell
       (get_input_volume_cmd, function(out, e, r, c)
-	  self.input_volume = get_volume_from_two_percents_string(out)
+         self.input_volume = get_volume_from_two_percents_string(out)
 
-	  self._refreshing_input_volume = false
-	  if not args.nonotify then
-	     self:emit_changed("input::volume")
-	  end
+         self._refreshing_input_volume = false
+         if not args.nonotify then
+            self:emit_changed("input::volume")
+         end
       end)
    end
 
@@ -127,12 +143,12 @@ function mk_sound_info()
 
       awful.spawn.easy_async_with_shell
       (get_input_volume_cmd, function(out, e, r, c)
-	  self.input_volume = get_volume_from_two_percents_string(out)
+         self.input_volume = get_volume_from_two_percents_string(out)
 
-	  self._refreshing_input_volume = false
-	  if not args.nonotify then
-	     self:emit_changed("input::volume")
-	  end
+         self._refreshing_input_volume = false
+         if not args.nonotify then
+            self:emit_changed("input::volume")
+         end
       end)
    end
 
@@ -142,8 +158,19 @@ function mk_sound_info()
 
       awful.spawn.easy_async_with_shell
       (get_sink_number_cmd, function(out, e, r, c)
-	  self.sink_number = out
-	  self._refreshing_sink_number = false
+         self.sink_number = out
+         self._refreshing_sink_number = false
+      end)
+   end
+
+   function sound_info:refresh_all_sinks_numbers(force)
+      if not force and self._refreshing_all_sinks_numbers then return end
+      self._refreshing_all_sinks_numbers = true
+
+      awful.spawn.easy_async_with_shell
+      (get_all_sinks_numbers_cmd, function(out, e, r, c)
+         self.all_sinks_numbers = split(out, "\n")
+         self._refreshing_all_sinks_numbers = false
       end)
    end
 
@@ -154,14 +181,40 @@ function mk_sound_info()
       self:refresh_output_type(args.force)
       self:refresh_output_volume(args or {})
       self:refresh_sink_number(args.force)
+      self:refresh_all_sinks_numbers(args.force)
    end
+
    function sound_info:get_input_device_volume() return self.input_volume end
+
    function sound_info:is_input_muted() return self.input_muted end
+
    function sound_info:get_output_device_volume() return self.output_volume end
+
    function sound_info:is_output_muted() return self.output_muted end
+
    function sound_info:get_output_device_type() return self.output_device_type end
+
    function sound_info:set_output_type_to_headphones() awful.spawn(set_output_type_to_headphones_cmd(self.sink_number)) end
+
    function sound_info:set_output_type_to_speakers() awful.spawn(set_output_type_to_speakers_cmd(self.sink_number)) end
+
+   function sound_info:set_sink(sink_number)
+      awful.spawn(set_sink_number_cmd(sink_number))
+      self.sink_number = sink_number
+   end
+
+   function sound_info:find_next_sink()
+      local prev_matched = false
+      for k, v in pairs(self.all_sinks_numbers) do
+         if prev_matched then
+            return v
+         end
+         if v == self.sink_number then
+            prev_matched = true
+         end
+      end
+      return self.all_sinks_numbers[1]
+   end
 
    return sound_info
 end
@@ -171,8 +224,8 @@ function mk_api(args)
    local sound_info = mk_sound_info()
    local refresh_timer = gears.timer {
       timeout = timeout,
-      callback = function ()
-	 sound_info:refresh { nonotify = true }
+      callback = function()
+         sound_info:refresh { nonotify = true }
       end,
       autostart = true,
    }
@@ -181,58 +234,62 @@ function mk_api(args)
       get_input_device_volume = function() return sound_info:get_input_device_volume() end,
       is_input_muted = function() return sound_info:is_input_muted() end,
       mute_input = function()
-	 awful.spawn(mute_input_cmd)
-	 sound_info:refresh_input_muted()
+         awful.spawn(mute_input_cmd)
+         sound_info:refresh_input_muted()
       end,
       unmute_input = function()
-	 awful.spawn(unmute_input_cmd)
-	 sound_info:refresh_input_muted()
+         awful.spawn(unmute_input_cmd)
+         sound_info:refresh_input_muted()
       end,
       toggle_mute_input = function()
-	 awful.spawn(toggle_mute_input_cmd)
-	 sound_info:refresh_input_muted()
+         awful.spawn(toggle_mute_input_cmd)
+         sound_info:refresh_input_muted()
       end,
       get_output_device_volume = function() return sound_info:get_output_device_volume() end,
       is_output_muted = function() return sound_info:is_output_muted() end,
       mute_output = function()
-	 awful.spawn(mute_output_cmd)
-	 sound_info:refresh_output_muted()
+         awful.spawn(mute_output_cmd)
+         sound_info:refresh_output_muted()
       end,
       unmute_output = function()
-	 awful.spawn(unmute_output_cmd)
-	 sound_info:refresh_output_muted()
+         awful.spawn(unmute_output_cmd)
+         sound_info:refresh_output_muted()
       end,
       toggle_mute_output = function()
-	 awful.spawn(toggle_mute_output_cmd)
-	 sound_info:refresh_output_muted()
+         awful.spawn(toggle_mute_output_cmd)
+         sound_info:refresh_output_muted()
       end,
       get_output_device_type = function() return sound_info:get_output_device_type() end,
       set_output_type_to_speakers = function()
-	 sound_info:set_output_type_to_speakers()
-	 sound_info:refresh_output_type()
+         sound_info:set_output_type_to_speakers()
+         sound_info:refresh_output_type()
       end,
       set_output_type_to_headphones = function()
-	 sound_info:set_output_type_to_headphones()
-	 sound_info:refresh_output_type()
+         sound_info:set_output_type_to_headphones()
+         sound_info:refresh_output_type()
       end,
       toggle_output_type = function()
-	 if sound_info.output_device_type == "speakers" then
-	    sound_info:set_output_type_to_headphones()
-	 else
-	    sound_info:set_output_type_to_speakers()
-	 end
-	 sound_info:refresh_output_type()
+         if sound_info.output_device_type == "speakers" then
+            sound_info:set_output_type_to_headphones()
+         else
+            sound_info:set_output_type_to_speakers()
+         end
+         sound_info:refresh_output_type()
+      end,
+      toggle_sink = function()
+         sound_info:set_sink(sound_info:find_next_sink())
+         sound_info:refresh_all_sinks_numbers()
       end,
       output_volume_up = function(x)
-	 awful.spawn(volume_up(x))
-	 sound_info:refresh_output_volume()
+         awful.spawn(volume_up(x))
+         sound_info:refresh_output_volume()
       end,
       output_volume_down = function(x)
-	 awful.spawn(volume_down(x))
-	 sound_info:refresh_output_volume()
+         awful.spawn(volume_down(x))
+         sound_info:refresh_output_volume()
       end,
       refresh = function(args)
-	 sound_info:refresh(args)
+         sound_info:refresh(args)
       end,
    }
 end
--- awesome/lib/widgets/sound.lua
+++ awesome/lib/widgets/sound.lua
@@ -25,54 +25,57 @@ function get_output_markup(args)
    local device_type = args.device_type
    local color = get_sound_icon_color(args.muted)
    local icon = icons[device_type]
-   return "<span foreground='" .. color .. "'>" .. icon .."</span>"
+   return "<span foreground='" .. color .. "'>" .. icon .. "</span>"
 end
 
 function get_input_markup(args)
    local color = get_sound_icon_color(args.muted)
    local icon = ""
-   return "<span foreground='" .. color .. "'>" .. icon .."</span>"
+   return "<span foreground='" .. color .. "'>" .. icon .. "</span>"
 end
 
 function mk_output_widget(api)
    local output = wibox.widget {
-      markup = nil,
-      align  = "center",
-      valign = "center",
-      font = font,
-      widget = wibox.widget.textbox,
+      markup  = nil,
+      align   = "center",
+      valign  = "center",
+      font    = font,
+      widget  = wibox.widget.textbox,
       buttons = gears.table.join(
-	 awful.button({}, 1, api.toggle_mute_output),
-	 awful.button({}, 3, api.toggle_output_type),
-	 awful.button({}, 4, function () api.output_volume_up(5) end),
-	 awful.button({}, 5, function () api.output_volume_down(5) end)
+         awful.button({}, 1, api.toggle_mute_output),
+         awful.button({}, 2, api.toggle_output_type),
+         awful.button({}, 3, api.toggle_sink),
+         awful.button({}, 4, function() api.output_volume_up(5) end),
+         awful.button({}, 5, function() api.output_volume_down(5) end)
       ),
    }
    function output:refresh()
       self.markup = get_output_markup {
-	 device_type = api.get_output_device_type(),
-	 muted = api.is_output_muted(),
+         device_type = api.get_output_device_type(),
+         muted = api.is_output_muted(),
       }
    end
+
    return output
 end
 
 function mk_input_widget(api)
    local input = wibox.widget {
-      markup = nil,
-      align  = "center",
-      valign = "center",
-      font = font,
-      widget = wibox.widget.textbox,
+      markup  = nil,
+      align   = "center",
+      valign  = "center",
+      font    = font,
+      widget  = wibox.widget.textbox,
       buttons = gears.table.join(
-	 awful.button({}, 1, api.toggle_mute_input)
+         awful.button({}, 1, api.toggle_mute_input)
       ),
    }
    function input:refresh()
       self.markup = get_input_markup {
-	 muted = api.is_input_muted(),
+         muted = api.is_input_muted(),
       }
    end
+
    return input
 end
 
@@ -88,8 +91,8 @@ end
 local function mk_tooltip(widget, percentage_getter)
    local sound_tooltip = awful.tooltip {
       objects = { widget },
-      timer_function = function ()
-	 return percentage_getter() .. "%"
+      timer_function = function()
+         return percentage_getter() .. "%"
       end,
    }
    sound_tooltip.margins = dpi(10)
@@ -101,9 +104,9 @@ function mk_widget()
    local input_widget = mk_input_widget(sound_api)
    local output_widget = mk_output_widget(sound_api)
    awesome.connect_signal("sound_api:changed:output",
-			  function () output_widget:refresh() end)
+      function() output_widget:refresh() end)
    awesome.connect_signal("sound_api:changed:input",
-			  function () input_widget:refresh() end)
+      function() input_widget:refresh() end)
    sound_api.refresh { nonotify = true }
    local output_tooltip = mk_tooltip(output_widget, sound_api.get_output_device_volume)
    local input_tooltip = mk_tooltip(input_widget, sound_api.get_input_device_volume)
