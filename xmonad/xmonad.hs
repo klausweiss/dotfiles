53f3fe0ad790e49b1e778ddeff34b7278ab73e92
Mikołaj Biel <mbiel@mailbox.org>
2023-11-25 00:04:55 +0100

    [xmonad] Basic startup programs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import UnliftIO.Directory (getHomeDirectory)
 import XMonad
 import XMonad.Config.Desktop
+import XMonad.Util.Cursor (setDefaultCursor)
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.SpawnOnce
 
@@ -11,14 +12,39 @@ main =
             , modMask = mod4Mask
             , startupHook = myStartupHook
             }
-            `additionalKeysP` [ ("M-p", spawn "rofi -show combi")
+            `additionalKeysP` [ ("M-p", spawn launcherCmd)
+                              , ("M-w", spawn browserCmd)
                               ]
+
+launcherCmd = "rofi -show combi"
+browserCmd = "firefox"
 
 myStartupHook :: X ()
 myStartupHook = do
-    spawnOnce "polybar"
-    spawnOnce "nm-applet"
-    spawnOnce "redshift -l 42.65:18.1"
+    setDefaultCursor xC_left_ptr
+    startBars
+    startRedshift
+    setWallpaper
+    populateTray
+    capslockIsCtrl
+
+capslockIsCtrl = do
+    spawnOnce "setxkbmap pl"
+    spawnOnce "setxkbmap -option shift:both_capslock"
+    spawnOnce "setxkbmap -option caps:ctrl_modifier"
+
+setWallpaper = do
     home <- getHomeDirectory
     let wallpaperPath = home <> "/.dotfiles/xorg/wallpaper.png"
     spawnOnce $ "feh --bg-fill " <> wallpaperPath
+
+startBars = do
+    spawnOnce "polybar"
+
+startRedshift = do
+    spawnOnce "redshift -l 42.65:18.1"
+
+populateTray = do
+    spawnOnce "nm-applet"
+    spawnOnce "blueman-applet"
+    spawnOnce "flameshot"
