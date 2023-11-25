16421c6794a5ced962c5b1fbbd609931ad1632a0
Mikołaj Biel <mbiel@mailbox.org>
2023-11-25 21:58:28 +0100

    [xmonad] Add some basic keyboard shortcurs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -12,12 +12,29 @@ main =
             , modMask = mod4Mask
             , startupHook = myStartupHook
             }
-            `additionalKeysP` [ ("M-p", spawn launcherCmd)
-                              , ("M-w", spawn browserCmd)
-                              ]
+            `additionalKeysP` myKeys
 
 launcherCmd = "rofi -show combi"
 browserCmd = "firefox"
+volumeUpCmd = "pactl set-sink-volume @DEFAULT_SINK@ +10%"
+volumeDownCmd = "pactl set-sink-volume @DEFAULT_SINK@ -10%"
+volumeMuteCmd = "pactl set-sink-mute @DEFAULT_SINK@ toggle"
+microphoneMuteCmd = "pactl set-source-mute @DEFAULT_SOURCE@ toggle"
+brightnessUpCmd = "lux -a 5%"
+brightnessDownCmd = "lux -s 5%"
+screenshotCmd = "flameshot gui"
+
+myKeys =
+    [ ("M-p", spawn launcherCmd)
+    , ("M-w", spawn browserCmd)
+    , ("<XF86MonBrightnessUp>", spawn brightnessUpCmd)
+    , ("<XF86MonBrightnessDown>", spawn brightnessDownCmd)
+    , ("<XF86AudioMute>", spawn volumeMuteCmd)
+    , ("<XF86AudioLowerVolume>", spawn volumeDownCmd)
+    , ("<XF86AudioRaiseVolume>", spawn volumeUpCmd)
+    , ("<XF86AudioMicMute>", spawn microphoneMuteCmd)
+    , ("<Print>", spawn screenshotCmd)
+    ]
 
 myStartupHook :: X ()
 myStartupHook = do
