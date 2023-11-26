68145e6e505a8079f2c2be9af8e09d7eae158865
Mikołaj Biel <mbiel@mailbox.org>
2023-11-26 22:18:44 +0100

    [xmonad] Start dunst, add some rules

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,12 +8,15 @@ import XMonad.Util.SpawnOnce
 main =
     xmonad $
         desktopConfig
-            { terminal = "kitty"
+            { terminal = terminalCmd
             , modMask = mod4Mask
             , startupHook = myStartupHook
+            , workspaces = myWorkspaces
+            , manageHook =  myManageHook <> manageHook desktopConfig
             }
             `additionalKeysP` myKeys
 
+terminalCmd = "kitty"
 launcherCmd = "rofi -show combi"
 browserCmd = "firefox"
 volumeUpCmd = "pactl set-sink-volume @DEFAULT_SINK@ +10%"
@@ -26,7 +29,8 @@ screenshotCmd = "flameshot gui"
 
 myKeys =
     [ ("M-p", spawn launcherCmd)
-    , ("M-w", spawn browserCmd)
+    , ("M-w", kill)
+    , ("M-S-w", spawn browserCmd)
     , ("<XF86MonBrightnessUp>", spawn brightnessUpCmd)
     , ("<XF86MonBrightnessDown>", spawn brightnessDownCmd)
     , ("<XF86AudioMute>", spawn volumeMuteCmd)
@@ -36,8 +40,30 @@ myKeys =
     , ("<Print>", spawn screenshotCmd)
     ]
 
+web = "1:web"
+dev = "2:dev"
+mail = "4:mail"
+myWorkspaces =
+    [ web
+    , dev
+    , "3"
+    , mail
+    ]
+        <> fmap show [4 .. 9]
+
+myManageHook =
+    mconcat
+        [ className =? "firefox" --> doShift web
+        , className =? "emacs" --> doShift dev
+        , className =? "Signal" --> doShift mail
+        , className =? "thunderbird" --> doShift mail
+        , className =? "zoom" --> doShift mail
+        , className =? "slack" --> doShift mail
+        ]
+
 myStartupHook :: X ()
 myStartupHook = do
+    startNotificationDaemon
     setDefaultCursor xC_left_ptr
     startBars
     startRedshift
@@ -54,6 +80,9 @@ setWallpaper = do
     home <- getHomeDirectory
     let wallpaperPath = home <> "/.dotfiles/xorg/wallpaper.png"
     spawnOnce $ "feh --bg-fill " <> wallpaperPath
+
+startNotificationDaemon = do
+    spawnOnce "dunst"
 
 startBars = do
     spawnOnce "polybar"
