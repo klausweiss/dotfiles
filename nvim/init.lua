a38740f741476a9c376d3ff9079a24f76cbc0be4
Mikołaj Biel <mbiel@mailbox.org>
2024-03-07 17:42:50 +0100

    [nvim] Use x for shrinking selection

--- nvim/init.lua
+++ nvim/init.lua
@@ -326,7 +326,7 @@ require 'nvim-treesitter.configs'.setup {
     keymaps = {
       node_incremental = "v",
       scope_incremental = "<leader>es",
-      node_decremental = "V",
+      node_decremental = "x",
     },
   },
 }
