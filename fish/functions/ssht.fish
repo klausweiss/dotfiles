c6bcb3267e3a1ccfc1f97d1c4f4f6943cd543ea3
Mikołaj Biel <mbiel@mailbox.org>
2024-05-03 12:49:11 +0200

    [fish] Fix ssht to use fish in the path

--- fish/functions/ssht.fish
+++ fish/functions/ssht.fish
@@ -1,4 +1,4 @@
 # Defined in /tmp/fish.7hb7JL/ssht.fish @ line 2
 function ssht
-	ssh -t $argv "env SHELL=/usr/bin/fish tmux new-session -A -s mbiel"
+	ssh -t $argv "env SHELL=`which fish` tmux new-session -A -s mbiel"
 end
