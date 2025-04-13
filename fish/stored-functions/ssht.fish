16b38839870e9d347208901bf111aa01dc561824
Mikołaj Biel <mbiel@mailbox.org>
2025-04-14 00:31:07 +0200

    [fish] Add TERM env var to ssht

--- fish/stored-functions/ssht.fish
+++ fish/stored-functions/ssht.fish
@@ -1,4 +1,3 @@
-# Defined in /tmp/fish.7hb7JL/ssht.fish @ line 2
 function ssht
-	ssh -t $argv "env SHELL=`which fish` tmux new-session -A -s mbiel"
+        ssh -t $argv "env SHELL=`which fish` TERM=xterm-256color tmux new-session -A -s mbiel"
 end
