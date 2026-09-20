0d957809f9618ec774e4bc4f23153079454b0244
Mikołaj Biel <mbiel@mailbox.org>
2026-09-20 23:46:07 +0200

    [neovim] Auto close neovim when the only window is nvim-tree

--- neovim/lua/config/lazy.lua
+++ neovim/lua/config/lazy.lua
@@ -67,10 +67,22 @@ require("lazy").setup({
 			dependencies = {
 				"nvim-tree/nvim-web-devicons",
 			},
-			opts = {},
 			keys = {
 				{ "<F1>", ":NvimTreeFindFileToggle<CR>" },
 			},
+			config = function()
+				require("nvim-tree").setup({})
+
+				-- Close nvim when nvim-tree is the last open window
+				vim.api.nvim_create_autocmd("BufEnter", {
+					nested = true,
+					callback = function()
+						if #vim.api.nvim_list_wins() == 1 and vim.bo.filetype == "NvimTree" then
+							vim.cmd("quit")
+						end
+					end,
+				})
+			end,
 		},
 	},
 	-- Configure any other settings here. See the documentation for more details.
