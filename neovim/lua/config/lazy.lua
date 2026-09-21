77fe66ce022096f92ebdf4e82551af6347dda3a0
Mikołaj Biel <mbiel@mailbox.org>
2026-09-21 22:09:37 +0200

    [neovim] Add obsidian.nvim and blink.cmp

--- neovim/lua/config/lazy.lua
+++ neovim/lua/config/lazy.lua
@@ -102,6 +102,102 @@ require("lazy").setup({
 				})
 			end,
 		},
+		{
+			"obsidian-nvim/obsidian.nvim",
+			version = "*", -- use latest release, remove to use latest commit
+			-- obsidian.nvim's UI features need conceallevel 1 or 2
+			init = function()
+				vim.api.nvim_create_autocmd("FileType", {
+					pattern = "markdown",
+					callback = function()
+						vim.opt_local.conceallevel = 1
+					end,
+				})
+			end,
+			---@module 'obsidian'
+			---@type obsidian.config
+			opts = {
+				legacy_commands = false, -- this will be removed in 4.0.0
+				workspaces = {
+					{
+						-- Dynamic workspace: <project>/wiki/src, found by walking up from the
+						-- current buffer (falling back to the cwd).
+						name = "project-wiki",
+						path = function()
+							local buf_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+							local start = (buf_dir and buf_dir ~= "") and buf_dir or vim.uv.cwd()
+							for dir in vim.fs.parents(start .. "/") do
+								local wiki = vim.fs.joinpath(dir, "wiki", "src")
+								if vim.fn.isdirectory(wiki) == 1 then
+									return wiki
+								end
+							end
+							return start
+						end,
+						overrides = {
+							-- daily/2026-09/2026-09-21.md
+							daily_notes = {
+								folder = "daily",
+								date_format = "YYYY-MM/YYYY-MM-DD",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			"saghen/blink.cmp",
+			-- optional: provides snippets for the snippet source
+			dependencies = { "rafamadriz/friendly-snippets" },
+
+			-- use a release tag to download pre-built binaries
+			version = "1.*",
+			-- AND/OR build from source
+			-- build = 'cargo build --release',
+			-- If you use nix, you can build from source with:
+			-- build = 'nix run .#build-plugin',
+
+			---@module 'blink.cmp'
+			---@type blink.cmp.Config
+			opts = {
+				-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+				-- 'super-tab' for mappings similar to vscode (tab to accept)
+				-- 'enter' for enter to accept
+				-- 'none' for no mappings
+				--
+				-- All presets have the following mappings:
+				-- C-space: Open menu or open docs if already open
+				-- C-n/C-p or Up/Down: Select next/previous item
+				-- C-e: Hide menu
+				-- C-k: Toggle signature help (if signature.enabled = true)
+				--
+				-- See :h blink-cmp-config-keymap for defining your own keymap
+				keymap = { preset = "default" },
+
+				appearance = {
+					-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+					-- Adjusts spacing to ensure icons are aligned
+					nerd_font_variant = "mono",
+				},
+
+				-- (Default) Only show the documentation popup when manually triggered
+				completion = { documentation = { auto_show = false } },
+
+				-- Default list of enabled providers defined so that you can extend it
+				-- elsewhere in your config, without redefining it, due to `opts_extend`
+				sources = {
+					default = { "lsp", "path", "snippets", "buffer" },
+				},
+
+				-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+				-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+				-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+				--
+				-- See the fuzzy documentation for more information
+				fuzzy = { implementation = "prefer_rust_with_warning" },
+			},
+			opts_extend = { "sources.default" },
+		},
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
